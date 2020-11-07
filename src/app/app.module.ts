d3238faac573076bbc17f7040a80dd245159ae67
VimukthiGunarathna <vimukthiappleid@gmail.com>
2020-11-07 09:53:58 +0530

    finish up top-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TopNavComponent } from './shared/top-nav/top-nav.component';
 import { DashboardComponent } from './public/dashboard/dashboard.component';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { faMinus, faPlus, faShoppingBag, faSortAmountDownAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faMinus, faPlus, faShoppingBag, faSortAmountDownAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -28,6 +28,6 @@ import { faMinus, faPlus, faShoppingBag, faSortAmountDownAlt } from '@fortawesom
 })
 export class AppModule {
   constructor(private library: FaIconLibrary) {
-    library.addIcons(faShoppingBag, faSortAmountDownAlt, faPlus, faMinus);
+    library.addIcons(faPlus, faCaretDown, faUser);
   }
 }
